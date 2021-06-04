0bb743a7ba26d4f2c20e32f571daaa18dd0f00d6
dhruv354 <dhruvsin@iitk.ac.in>
2021-06-04 19:13:39 +0530

    working with default database sqlite3

--- Statictut/models.py
+++ Statictut/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class details(models.Model):
+    name =  models.CharField(max_length=30)
+    img = models.ImageField(default='')
+    age = models.IntegerField(default=-1)
+    pub_date = models.DateField()
+
