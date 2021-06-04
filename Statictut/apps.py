d81051aa7b79d406c021ad9770f8896a3e396702
dhruv354 <dhruvsin@iitk.ac.in>
2021-06-04 14:44:08 +0530

    learnt how to load static files in django

---
+++ Statictut/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class StatictutConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Statictut'
--- myApp/urls.py
+++ myApp/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('tut.urls')),
+    path('', include('Statictut.urls')),
     path('', include('tut.urls'))
 ]
