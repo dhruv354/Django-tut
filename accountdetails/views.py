59d6891dc9dcb1fca3417090b117330bdcbd8de6
dhruv354 <dhruvsin@iitk.ac.in>
2021-06-05 11:29:01 +0530

    added messages

--- accountdetails/views.py
+++ accountdetails/views.py
@@ -1,6 +1,7 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
+from django.contrib import messages
 # Create your views here.
 
 def register(request):
@@ -11,6 +12,9 @@ def register(request):
         password = request.POST.get('password')
         confirm_password = request.POST.get('confirm_password')
 
+        if(password != confirm_password):
+             messages.info(request, 'passwords does not match')
+             return redirect('register')
         user = User.objects.create_user(username=name, password=password, email=email)
         user.save()
         print('user created')
