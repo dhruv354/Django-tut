25397139daaa699f73147717a537283bebdbfa5b
dhruv354 <dhruvsin@iitk.ac.in>
2021-06-05 11:02:37 +0530

    created a non-superuser and do authentication

--- accountdetails/views.py
+++ accountdetails/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
-
+from django.http.response import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
 # Create your views here.
+
+def register(request):
+
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm_password')
+
+        user = User.objects.create_user(username=name, password=password, email=email)
+        user.save()
+        print('user created')
+
+        return redirect('/')
+    else:
+        return render(request, 'register.html')
--- myApp/urls.py
+++ myApp/urls.py
@@ -21,5 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Statictut.urls')),
-    path('', include('tut.urls'))
+    path('', include('tut.urls')),
+    path('accounts/', include('accountdetails.urls'))
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
